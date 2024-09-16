2be970a8733f67c1fe5a81b86962706aa6b0dcbc
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-09-16 17:17:38 -0400

    I hope you're not Board already

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Board.java
@@ -0,0 +1,93 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+import java.util.HashMap;
+
+public class Board {
+    IMU imu = null;
+    private final DcMotor[] drivebase = {null, null, null, null};
+
+    public void init(HardwareMap hwMap) {
+        HashMap<String, Throwable> fails = new HashMap<>();
+
+        try {
+            drivebase[0] = hwMap.get(DcMotor.class, "fl");
+            drivebase[1] = hwMap.get(DcMotor.class, "fr");
+            drivebase[2] = hwMap.get(DcMotor.class, "br");
+            drivebase[3] = hwMap.get(DcMotor.class, "bl");
+
+            drivebase[0].setDirection(DcMotorSimple.Direction.REVERSE);
+            drivebase[1].setDirection(DcMotorSimple.Direction.FORWARD);
+            drivebase[2].setDirection(DcMotorSimple.Direction.FORWARD);
+            drivebase[3].setDirection(DcMotorSimple.Direction.REVERSE);
+        } catch (Throwable e) {
+            fails.put("The Drivebase", e);
+        }
+
+        try {
+            imu = hwMap.get(IMU.class, "imu");
+
+            imu.initialize( //TODO: GET VALUES GET VALUES GET VALUES GET VALUES (subtle, eh?)
+                    new IMU.Parameters(
+                            new RevHubOrientationOnRobot(
+                                    null,
+                                    null
+                            )
+                    )
+            );
+        } catch (Throwable e) {
+            fails.put("IMU", e);
+        }
+    }
+
+    double getAngle() {
+        return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+    }
+
+    public void setPowers(double flp, double frp, double blp, double brp) {
+        double maxSpeed = 1;
+
+        maxSpeed = Math.max(maxSpeed, Math.abs(flp));
+        maxSpeed = Math.max(maxSpeed, Math.abs(frp));
+        maxSpeed = Math.max(maxSpeed, Math.abs(brp));
+        maxSpeed = Math.max(maxSpeed, Math.abs(blp));
+
+        flp /= maxSpeed;
+        frp /= maxSpeed;
+        brp /= maxSpeed;
+        blp /= maxSpeed;
+
+        drivebase[0].setPower(flp);
+        drivebase[1].setPower(frp);
+        drivebase[2].setPower(brp);
+        drivebase[3].setPower(blp);
+    }
+
+    public void drive(double forward, double right, double rotate) {
+        double flp = forward + right + rotate;
+        double frp = forward - right - rotate;
+        double blp = forward - right + rotate;
+        double brp = forward + right - rotate;
+
+        setPowers(flp, frp, blp, brp);
+    }
+
+    public void driveFieldRelative(double forward, double right, double rotate) {
+        double theta = Math.atan2(forward, right);
+        double r = Math.hypot(forward, right);
+
+        theta = AngleUnit.normalizeRadians(theta - getAngle());
+
+        double newForward = r * Math.sin(theta);
+        double newRight = r * Math.cos(theta);
+
+        drive(newForward, newRight, rotate);
+    }
+}
