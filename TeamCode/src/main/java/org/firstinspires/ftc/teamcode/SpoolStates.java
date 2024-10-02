c250966923d0b96b9fd416b013259324c40d6f56
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-10-02 18:17:26 -0400

    full const list

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpoolStates.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpoolStates.java
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 public final class SpoolStates {
-    public final static int MINIMUM = 0, //TODO: set others, and make these accurate
-    MAXIMUM = 100;
+    public final static int MINIMUM = 0,
+    CHAMBERLOW = 1,
+    CHAMBERHIGH = 1,
+    BUCKETLOW = 1,
+    BUCKETHIGH = 1,
+    MAXIMUM = 100; //TODO: WHAT ACTUALLY ARE THESE
 }
