af5a8f4a8818f26f2efd4ac55cf0657247f76942
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-10-02 17:21:34 -0400

    enum failed, this

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpoolStates.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+
+public final class SpoolStates {
+    final static int MINIMUM = 0;
+//TODO: set others, and make these accurate
+    final static int MAXIMUM = 100;
+}
