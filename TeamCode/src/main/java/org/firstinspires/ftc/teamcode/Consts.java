be31fd89330306d231728bc10a3a37e4b3dabf1b
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-11-06 11:03:36 -0500

    Borrowing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Consts.java
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-/** Magic Numbers! */
-public final class Consts {
-}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TestAuto.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Angle;
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.Vector2d;
+
+@Autonomous(name = "Diff Swerve Test Auto", group = "Linear Opmode")
+
+public class TestAuto extends LinearOpMode {
+    Robot robot;
+
+    public void runOpMode() {
+        robot = new Robot(this, true);
+        robot.initIMU();
+
+        //simple sequence to demonstrate the three main autonomous primitives
+
+        //rotate modules to face to the right
+        robot.driveController.rotateModules(Vector2d.RIGHT, false,4000, this);
+
+        //drive 20 cm to the right (while facing forward)
+        robot.driveController.drive(Vector2d.RIGHT, 20, 1, this);
+
+        //turn to face robot right
+        robot.driveController.rotateRobot(Angle.RIGHT, this);
+    }
+}
