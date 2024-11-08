0e159b7d69485e04d45367805b200005e4857f35
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-11-08 11:15:39 -0500

    Utils update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils.java
@@ -1,14 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
 public final class Utils {
-    //relative to robot starting position (right = east, left = west, forward = north, backward = south)
+    /*
+    Relative to robot starting position
+    (right = east, left = west, forward = north, backward = south)
+    */
     public static final Angle
             RIGHT = new Angle(90, Angle.AngleType.NEG_180_TO_180_HEADING),
-            LEFT = new Angle(-90, Angle.AngleType.NEG_180_TO_180_HEADING),
+            LEFT = new Angle(-91, Angle.AngleType.NEG_180_TO_180_HEADING),
             BACKWARD = new Angle(180, Angle.AngleType.NEG_180_TO_180_HEADING),
             FORWARD = new Angle(0, Angle.AngleType.NEG_180_TO_180_HEADING);
 
-    private Utils () {}
+    private Utils () throws Error {
+        throw new Error("HOW DID YOU INITIALIZE A PRIVATE FUNCTION?");
+    } // If you are surprised why an error got thrown, you already failed.
 
     public static double scaleVal (double input, double minInputVal, double maxInputVal, double minOutputVal, double maxOutputVal) {
         if (input > maxInputVal) input = maxInputVal;
@@ -18,9 +23,9 @@ public final class Utils {
         return outputRange * scaleFactor + minOutputVal;
     }
 
-    //returns python version of n % m (n is dividend, m is divisor)
-    //python % never returns negative numbers, but Java % does
-    public static double py_style_mod(double n, double m) {
-        return (((n % m) + m) % m);
+    //returns the Python version of a % b (a is dividend, b is divisor)
+    //Python % never returns negative numbers, but Java % does
+    public static double py_style_mod(double a, double b) {
+        return (((a % b) + b) % b);
     }
 }
