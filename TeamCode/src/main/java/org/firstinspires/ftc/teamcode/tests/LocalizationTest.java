76f878ca5ec92b03616e4570b611b08cb98fb20c
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-11-12 16:18:42 -0500

    Premature optimization may be the root of all evil, but it sure is fun!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/LocalizationTest.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.extLib.Board;
+import org.firstinspires.ftc.teamcode.extLib.wheelieExt.PathFollowerWrapper;
+
+import Wheelie.Pose2D;
+
+@TeleOp(group = "Test")
+public class LocalizationTest extends LinearOpMode {
+    Board board = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        PathFollowerWrapper drive = new PathFollowerWrapper(hardwareMap, new Pose2D(0, 0, Math.PI/2.), 8);
+        board = new Board(hardwareMap);
+        telemetry.addLine("Initialized");
+        telemetry.addLine(drive.getPoseString());
+        telemetry.addLine(drive.getPoseString());
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            drive.updatePose(board.getAngle());
+            telemetry.addLine(drive.getPoseString());
+            telemetry.addData("Horizontal wheel", drive.getHoriOdom());
+            telemetry.addData("Vertical wheel", drive.getVertOdom());
+            telemetry.update();
+        }
+    }
+}
