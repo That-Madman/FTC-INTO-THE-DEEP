74c81a7da2c678e0821d010e98b6e320874398e8
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-11-14 17:08:04 -0500

    so this exists

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/proto.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class proto extends OpMode {
+
+    CRServo grips [] = {null, null};
+
+    @Override
+    public void init () {
+        grips[0] = hardwareMap.get(CRServo.class, "s1");
+        grips[1] = hardwareMap.get(CRServo.class, "s2");
+
+        grips[1].setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop () {
+        if (gamepad1.a) {
+           grips[0].setPower(1);
+           grips[1].setPower(1);
+        }
+    }
+}
