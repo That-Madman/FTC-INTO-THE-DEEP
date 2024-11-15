e94469cdbe3325bb4f12c65e96b5b0d32b292eb9
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-11-15 18:17:20 -0500

    added basic driving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/MainTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/MainTele.java
@@ -5,21 +5,71 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.extLib.hardware.Board;
 
-@TeleOp (name = "The one you use")
+@TeleOp(name = "The one you use")
 public class MainTele extends OpMode {
     Board board;
 
+    boolean bHeld;
+    boolean driveFieldRel = true;
+    boolean resetImu;
+    boolean yHeld;
+
     @Override
-    public void init () {
+    public void init() {
         board = new Board(hardwareMap);
     }
 
     @Override
-    public void loop () {
-        board.driveFieldRelative(
-                -gamepad1.left_stick_y,
-                gamepad1.left_stick_x,
-                gamepad1.right_stick_x
-        );
+    public void init_loop() {
+        if (gamepad1.y && yHeld) {
+            resetImu ^= true; // Inverts variable. Why do it like this? Because it's fun.
+        }
+
+        telemetry.clear();
+        telemetry.addLine("The IMU will be " + (resetImu ? "reset" : "unchanged") + ".");
+        telemetry.update();
+
+        yHeld = gamepad1.y;
+    }
+
+    @Override
+    public void start() {
+        if (resetImu) {
+            board.resetImu();
+            resetImu = false;
+        }
+    }
+
+    @Override
+    public void loop() {
+        if (driveFieldRel) {
+            board.driveFieldRelative(
+                    -gamepad1.left_stick_y,
+                    gamepad1.left_stick_x,
+                    gamepad1.right_stick_x
+            );
+        } else {
+            board.drive(
+                    -gamepad1.left_stick_y,
+                    gamepad1.left_stick_x,
+                    gamepad1.right_stick_x
+            );
+        }
+
+        if (gamepad1.b && bHeld) {
+            driveFieldRel ^= true;
+        }
+
+        if (gamepad1.y && yHeld) {
+            board.resetImu();
+            resetImu = true;
+        }
+
+        if (resetImu) {
+            telemetry.addLine("IMU has been reset.");
+        }
+
+        bHeld = gamepad1.b;
+        yHeld = gamepad1.y;
     }
 }
