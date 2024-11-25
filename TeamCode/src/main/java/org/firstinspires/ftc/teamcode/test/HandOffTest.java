5255bcc94152b05218cd4cfa5677aa28d7b6b01f
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-11-25 13:42:30 -0500

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/HandOffTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/HandOffTest.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
-
 @TeleOp (group = "Tests")
 public class HandOffTest extends LinearOpMode {
     Servo pick;
