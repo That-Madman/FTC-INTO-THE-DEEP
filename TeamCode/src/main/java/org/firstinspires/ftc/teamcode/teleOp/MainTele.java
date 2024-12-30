0cde707b6c5a4fbf77250dc677abc8cd42aac5ed
That-Madman <144173821+That-Madman@users.noreply.github.com>
2024-12-30 11:55:09 -0500

    Ch-Ch-Changes!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/MainTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/MainTele.java
@@ -171,7 +171,7 @@ public class MainTele extends OpMode {
         }
 
         if (!rB2Held && gamepad2.right_bumper) {
-            rot = (byte) ((0 < rot) ? 0 : 1);
+            rot = (byte) ((0 < rot) ? 0 : 1); //TODO: I DON'T THINK THIS WORKS
         }
 
         if (gamepad2.dpad_up && !up2Held) {
