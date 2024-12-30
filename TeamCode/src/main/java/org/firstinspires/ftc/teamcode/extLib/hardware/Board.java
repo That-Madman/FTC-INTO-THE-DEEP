375d2a9ce421528a87985c17d09eaa9a9ef2203b
That-Madman <144173821+That-Madman@users.noreply.github.com>
2024-12-30 12:10:32 -0500

    Ch-Ch-Changes!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extLib/hardware/Board.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extLib/hardware/Board.java
@@ -198,7 +198,7 @@ public class Board {
                 swivel.setPosition(0.5);
                 break;
             case 2:
-                swivel.setPosition(0.8);
+                swivel.setPosition(0.7);
                 break;
         }
     }
