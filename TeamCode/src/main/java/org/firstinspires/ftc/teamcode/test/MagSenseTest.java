4ed4fc24a5d45ad8667494ac3bb2dc799af3a18a
That-Madman <144173821+That-Madman@users.noreply.github.com>
2025-01-02 09:55:12 -0500

    I commented out two lines

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/MagSenseTest.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+@TeleOp (group = "Tests")
+public class MagSenseTest extends OpMode {
+    private TouchSensor v1t, v2t;
+
+    @Override
+    public void init() {
+        v1t = hardwareMap.get(TouchSensor.class, "v1t");
+        v2t = hardwareMap.get(TouchSensor.class, "v2t");
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("v1t", !v1t.isPressed());
+        telemetry.addData("v2t", !v2t.isPressed());
+    }
+}
