fa63458dada92328c01ac60df457761f1581c6ab
That-Madman <144173821+That-Madman@users.noreply.github.com>
2025-01-03 09:24:43 -0500

    Chipper's Revenge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extLib/hardware/Board.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extLib/hardware/Board.java
@@ -13,21 +13,9 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class Board {
-    Servo bigGrab;
-    Servo lRot;
-    Servo mRot;
-    Servo pick;
-    Servo r1, r2;
-    Servo rRot;
-    Servo swivel;
-    Servo tinyGrab;
-
-    private final DcMotorEx v1, v2;
-    private final DcMotor[] base = {null, null, null, null};
+   private final DcMotor[] base = {null, null, null, null};
 
     private final IMU imu;
-
-    private final TouchSensor v1t, v2t;
 
     public Board (HardwareMap hwMap) {
             base[0] = hwMap.get(DcMotor.class, "fl");
@@ -49,21 +37,6 @@ public class Board {
             base[3].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             base[3].setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-            v1 = hwMap.get(DcMotorEx.class, "v1");
-            v2 = hwMap.get(DcMotorEx.class, "v2");
-
-            v1.setDirection(DcMotorSimple.Direction.REVERSE);
-            v2.setDirection(DcMotorSimple.Direction.FORWARD);
-
-            v1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            v2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            v1.setPower(0.7);
-            v2.setPower(0.7);
-            v1.setTargetPosition(0);
-            v2.setTargetPosition(0);
-            v1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            v2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
             imu = hwMap.get(IMU.class, "imu");
 
             imu.initialize(
@@ -74,146 +47,17 @@ public class Board {
                             )
                     )
             );
-
-        tinyGrab = hwMap.get(Servo.class, "tinyGrab");
-        bigGrab = hwMap.get(Servo.class, "bigGrab");
-        lRot = hwMap.get(Servo.class, "lRot");
-        rRot = hwMap.get(Servo.class, "rRot");
-        mRot = hwMap.get(Servo.class, "mRot");
-
-        lRot.setDirection(Servo.Direction.REVERSE);
-        setRot((byte) 0);
-        setTinyGrab(false);
-        setBigGrab(false);
-
-        r1 = hwMap.get(Servo.class, "reachy");
-        r2 = hwMap.get(Servo.class, "reachier");
-
-        r2.setDirection(Servo.Direction.REVERSE);
-
-        pick = hwMap.get(Servo.class, "pick");
-        swivel = hwMap.get(Servo.class, "swivel");
-
-        setPick(false);
-        setSwivel((byte) 1);
-
-        //TODO: ASSIGN ME ASSIGN ME ASSIGN ME ASSIGN ME ASSIGN ME ASSIGN ME ASSIGN ME ASSIGN ME
-        v1t = null;
-        v2t = null;
-    }
-
-    public double getAngle() {
-        return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-    }
-
-    public int getLiftPos (boolean rl) {
-        return (rl ? v1 : v2).getCurrentPosition();
-    }
-
-    public void resetLift (boolean rl) {
-        if (rl) {
-            v1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            v1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        } else {
-            v2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            v2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        }
-    }
-
-    public boolean getLiftTouched (boolean rl) {
-        return !(rl ? v1t : v2t).isPressed();
-    }
-
-    public void setLiftDampen (float damp) {
-            v1.setPower(damp);
-            v2.setPower(damp);
-    }
+   }
 
     public void resetImu () {
         imu.resetYaw();
     }
 
-    public void setLift (int height) {
-        v1.setTargetPosition(height);
-        v2.setTargetPosition(height);
+    public double getDeg () {
+        return imu.getRobotYawPitchRollAngles().getYaw();
     }
 
-    public void setReach (byte r) {
-        switch (r) {
-            case 0:
-                r1.setPosition(1);
-                r2.setPosition(1);
-                break;
-
-            case 1:
-                r1.setPosition(0.5);
-                r2.setPosition(0.5);
-                break;
-
-            case 2:
-                r1.setPosition(0.1);
-                r2.setPosition(0.1);
-                break;
-        }
-    }
-
-    public void setTinyGrab (boolean c) {
-        tinyGrab.setPosition(c ? 0.25 : 1);
-    }
-
-    public void setBigGrab (boolean c) {
-        bigGrab.setPosition(c ? 1 : 0.5);
-    }
-
-    public void setRot (byte u) {
-        switch (u) {
-            case 0:
-                lRot.setPosition(0.1);
-                rRot.setPosition(0.1);
-                mRot.setPosition(0);
-                break;
-            case 1:
-                lRot.setPosition(1);
-                rRot.setPosition(1);
-                mRot.setPosition(0);
-                break;
-            case 2:
-                lRot.setPosition(0.8);
-                rRot.setPosition(0.8);
-                mRot.setPosition(1);
-                break;
-            case 4:
-                lRot.setPosition(0.7);
-                rRot.setPosition(0.7);
-                mRot.setPosition(0);
-                break;
-            case 3:
-                lRot.setPosition(0.2);
-                rRot.setPosition(0.2);
-                mRot.setPosition(1);
-                break;
-        }
-    }
-
-    public void setPick (boolean c) {
-        pick.setPosition(c ? 0.5 : 1);
-    }
-
-    public void setSwivel (byte s) {
-        switch (s) {
-            case 0:
-                swivel.setPosition(0.2);
-                break;
-            case 1:
-                swivel.setPosition(0.5);
-                break;
-            case 2:
-                swivel.setPosition(1);
-                break;
-        }
-    }
-
-    public void sleep (long millis, OpMode op) {
+   public void sleep (long millis, OpMode op) { //TODO: IS THIS STILL NEEDED?
         op.resetRuntime();
         while (op.getRuntime() < (double) millis / 1000);
     }
@@ -248,7 +92,7 @@ public class Board {
 
     public void driveFieldRelative(double forward, double right, double rotate) {
         final double r = Math.hypot(forward, right);
-        final double theta = AngleUnit.normalizeRadians(Math.atan2(forward, right) - getAngle());
+        final double theta = AngleUnit.normalizeRadians(Math.atan2(forward, right) - getDeg());
 
         drive(r * Math.sin(theta), r * Math.cos(theta), rotate);
     }
