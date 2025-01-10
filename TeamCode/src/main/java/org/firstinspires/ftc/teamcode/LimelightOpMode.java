10b960ad8bc4288a20c139e81736ea7090e50af7
PlagueDoctorSadMan <karakashg@franklinacademy.org>
2025-01-10 11:42:05 -0500

    Limelight functionality test wooooo...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightOpMode.java
@@ -1,14 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.limelightvision.LLResult;
-import com.qualcomm.hardware.limelightvision.LLResultTypes;
-import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-
-import java.util.List;
 @TeleOp
 public class LimelightOpMode extends OpMode{
     int tag;
