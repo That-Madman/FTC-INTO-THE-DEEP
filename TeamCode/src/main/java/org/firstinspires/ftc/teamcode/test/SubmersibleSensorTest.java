1fdf1199685a7dbd11b2503f5d857e57cd939c07
That-Madman <144173821+That-Madman@users.noreply.github.com>
2025-01-13 17:04:11 -0500

    removed whitespace

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/SubmersibleSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/SubmersibleSensorTest.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.test;
 
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.LED;
