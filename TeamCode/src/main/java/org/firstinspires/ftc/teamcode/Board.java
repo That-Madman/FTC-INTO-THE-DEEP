abb4474b8b45587a3d4e29fa7473a9e1bd92ac97
That-Madman <144173821+That-Madman@users.noreply.github.com>
2025-01-14 17:13:07 -0500

    remove dead code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Board.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Board.java
@@ -1,172 +1,220 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
-import java.io.FileWriter;
-import java.util.HashMap;
-import java.util.Objects;
-
-import Wheelie.PID;
-
 public class Board {
-    private final DcMotor[] drivebase = {null, null, null, null};
-    private DcMotorEx spool = null;
+    Servo bigGrab;
+    Servo lRot;
+    Servo mRot;
+    Servo pick;
+    Servo r1, r2;
+    Servo rRot;
+    Servo swivel;
+    Servo tinyGrab;
 
-    private Servo claw = null;
-    private Servo horzExt = null;
+    private final DcMotorEx v1, v2;
+    private final DcMotor[] base = {null, null, null, null};
 
-    private CRServo sweep = null;
-    private CRServo sweep2 = null;
+    private final IMU imu;
 
-    private IMU imu = null;
-
-    private final PID spoolPID = new PID(0.1, 0, 0.8);
+    private final TouchSensor v1t, v2t;
 
     public Board (HardwareMap hwMap) {
-        HashMap<String, Throwable> fails = new HashMap<>();
+            base[0] = hwMap.get(DcMotor.class, "fl");
+            base[1] = hwMap.get(DcMotor.class, "fr");
+            base[2] = hwMap.get(DcMotor.class, "br");
+            base[3] = hwMap.get(DcMotor.class, "bl");
 
-        try {
-            drivebase[0] = hwMap.get(DcMotor.class, "fl");
-            drivebase[1] = hwMap.get(DcMotor.class, "fr");
-            drivebase[2] = hwMap.get(DcMotor.class, "br");
-            drivebase[3] = hwMap.get(DcMotor.class, "bl");
+            base[0].setDirection(DcMotorSimple.Direction.REVERSE);
+            base[1].setDirection(DcMotorSimple.Direction.FORWARD);
+            base[2].setDirection(DcMotorSimple.Direction.REVERSE);
+            base[3].setDirection(DcMotorSimple.Direction.REVERSE);
 
-            drivebase[0].setDirection(DcMotorSimple.Direction.REVERSE);
-            drivebase[1].setDirection(DcMotorSimple.Direction.FORWARD);
-            drivebase[2].setDirection(DcMotorSimple.Direction.FORWARD);
-            drivebase[3].setDirection(DcMotorSimple.Direction.REVERSE);
+            base[0].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            base[0].setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            base[1].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            base[1].setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            base[2].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            base[2].setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            base[3].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            base[3].setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-            drivebase[0].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            drivebase[0].setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            drivebase[1].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            drivebase[1].setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            drivebase[2].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            drivebase[2].setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            drivebase[3].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            drivebase[3].setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        } catch (Throwable e) {
-            fails.put("The Drivebase", e);
-        }
+            v1 = hwMap.get(DcMotorEx.class, "v1");
+            v2 = hwMap.get(DcMotorEx.class, "v2");
 
-        try {
-            spool = hwMap.get(DcMotorEx.class, "spool");
+            v1.setDirection(DcMotorSimple.Direction.REVERSE);
+            v2.setDirection(DcMotorSimple.Direction.FORWARD);
 
-            spool.setDirection(DcMotorSimple.Direction.FORWARD);
-            spool.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        } catch (Throwable e) {
-            fails.put("Spool Motor", e);
-        }
+            v1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            v2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            v1.setPower(0.7);
+            v2.setPower(0.7);
+            v1.setTargetPosition(0);
+            v2.setTargetPosition(0);
+            v1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            v2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        try {
-            claw = hwMap.get(Servo.class, "gripper");
-        } catch (Throwable e) {
-            fails.put("Gripper", e);
-        }
-
-        try {
-            horzExt = hwMap.get(Servo.class, "arm");
-        } catch (Throwable e) {
-            fails.put("Horizontal Extension", e);
-        }
-
-        try {
-            sweep = hwMap.get(CRServo.class, "sweeper");
-        } catch (Throwable e) {
-            fails.put("The Sweeper", e);
-        }
-
-        try {
-            sweep2 = hwMap.get(CRServo.class, "sweep2");
-        } catch (Throwable e) {
-            fails.put("The Sweeper 2", e);
-        }
-
-        try {
             imu = hwMap.get(IMU.class, "imu");
 
             imu.initialize(
                     new IMU.Parameters(
                             new RevHubOrientationOnRobot(
-                                    RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                                    RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+                                    RevHubOrientationOnRobot.LogoFacingDirection.DOWN,
+                                    RevHubOrientationOnRobot.UsbFacingDirection.RIGHT
                             )
                     )
             );
-        } catch (Throwable e) {
-            fails.put("IMU", e);
-        }
 
-        try {
-            FileWriter writer = new FileWriter("BOARD_LOG.txt", false);
+        tinyGrab = hwMap.get(Servo.class, "tinyGrab");
+        bigGrab = hwMap.get(Servo.class, "bigGrab");
+        lRot = hwMap.get(Servo.class, "lRot");
+        rRot = hwMap.get(Servo.class, "rRot");
+        mRot = hwMap.get(Servo.class, "mRot");
 
-            for (String dev : fails.keySet()) {
-                writer.write(dev + " : " + Objects.requireNonNull(fails.get(dev)) + "\n");
-            }
+        lRot.setDirection(Servo.Direction.REVERSE);
+        setRot((byte) 0);
+        setTinyGrab(false);
+        setBigGrab(false);
 
-            writer.close();
-        } catch (Throwable ignored) {
-        }
-    }
+        r1 = hwMap.get(Servo.class, "reachy");
+        r2 = hwMap.get(Servo.class, "reachier");
 
-    public void setSpoolPower (double power) {
-        spool.setPower(power);
-    }
+        r2.setDirection(Servo.Direction.REVERSE);
 
-    public int getSpoolPosition () {
-        return spool.getCurrentPosition();
-    }
+        pick = hwMap.get(Servo.class, "pick");
+        swivel = hwMap.get(Servo.class, "swivel");
 
-    public void autoSpoolOverride () {
-        spool.setTargetPosition(0);
-        spool.setPower(0.75);
-        spool.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-    }
+        setPick(false);
+        setSwivel((byte) 1);
 
-    public void spoolTo (int pos) {
-        spool.setTargetPosition(pos);
-    }
-
-   public void setSpoolPos (int pos) {
-        spool.setPower(spoolPID.pidCalc(pos, getSpoolPosition()));
-   }
-
-    public void setClaw (boolean open) {
-        if (open) {
-            claw.setPosition(0); //open position, according to Caleb
-        } else {
-            claw.setPosition(1); //thus follows that this is the closed position
-        }
-    }
-
-    public void setHorzExt (short ext) {
-        switch (ext) {
-            case 0:
-                horzExt.setPosition(0);
-                break;
-            case 1:
-                horzExt.setPosition(0.5);
-                break;
-            case 2:
-                horzExt.setPosition(1);
-        }
-    }
-
-    public void setSweep (double pow) {
-        sweep.setPower(-pow);
-        sweep2.setPower(pow);
+        v1t = hwMap.get(TouchSensor.class, "v1t");
+        v2t = hwMap.get(TouchSensor.class, "v2t");
     }
 
     public double getAngle() {
         return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+    }
+
+    public int getLiftPos (boolean rl) {
+        return (rl ? v1 : v2).getCurrentPosition();
+    }
+
+    public void resetLift (boolean rl) {
+        if (rl) {
+            v1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            v1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        } else {
+            v2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            v2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+    }
+
+    public boolean getLiftTouched (boolean rl) {
+        return (rl ? v1t : v2t).isPressed();
+    }
+
+    public void setLiftDampen (float damp) {
+            v1.setPower(damp);
+            v2.setPower(damp);
+    }
+
+    public void resetImu () {
+        imu.resetYaw();
+    }
+
+    public void setLift (int height) {
+        v1.setTargetPosition(height);
+        v2.setTargetPosition(height);
+    }
+
+    public void setReach (byte r) {
+        switch (r) {
+            case 0:
+                r1.setPosition(1);
+                r2.setPosition(1);
+                break;
+
+            case 1:
+                r1.setPosition(0.5);
+                r2.setPosition(0.5);
+                break;
+
+            case 2:
+                r1.setPosition(0.1);
+                r2.setPosition(0.1);
+                break;
+        }
+    }
+
+    public void setTinyGrab (boolean c) {
+        tinyGrab.setPosition(c ? 0.25 : 1);
+    }
+
+    public void setBigGrab (boolean c) {
+        bigGrab.setPosition(c ? 1 : 0.5);
+    }
+
+    public void setRot (byte u) {
+        switch (u) {
+            case 0:
+                lRot.setPosition(0.1);
+                rRot.setPosition(0.1);
+                mRot.setPosition(0);
+                break;
+            case 1:
+                lRot.setPosition(1);
+                rRot.setPosition(1);
+                mRot.setPosition(0);
+                break;
+            case 2:
+                lRot.setPosition(0.75);
+                rRot.setPosition(0.75);
+                mRot.setPosition(1);
+                break;
+            case 4:
+                lRot.setPosition(0.7);
+                rRot.setPosition(0.7);
+                mRot.setPosition(0);
+                break;
+            case 3:
+                lRot.setPosition(0.2);
+                rRot.setPosition(0.2);
+                mRot.setPosition(1);
+                break;
+        }
+    }
+
+    public void setPick (boolean c) {
+        pick.setPosition(c ? 0.5 : 1);
+    }
+
+    public void setSwivel (byte s) {
+        switch (s) {
+            case 0:
+                swivel.setPosition(0.2);
+                break;
+            case 1:
+                swivel.setPosition(0.5);
+                break;
+            case 2:
+                swivel.setPosition(1);
+                break;
+        }
+    }
+
+    public void sleep (long millis, OpMode op) {
+        op.resetRuntime();
+        while (op.getRuntime() < (double) millis / 1000);
     }
 
     public void setPowers(double flp, double frp, double blp, double brp) {
@@ -182,34 +230,25 @@ public class Board {
         brp /= maxSpeed;
         blp /= maxSpeed;
 
-        drivebase[0].setPower(flp);
-        drivebase[1].setPower(frp);
-        drivebase[2].setPower(brp);
-        drivebase[3].setPower(blp);
+        base[0].setPower(flp);
+        base[1].setPower(frp);
+        base[2].setPower(brp);
+        base[3].setPower(blp);
     }
 
     public void drive(double forward, double right, double rotate) {
-        double flp = forward + right + rotate;
-        double frp = forward - right - rotate;
-        double blp = forward - right + rotate;
-        double brp = forward + right - rotate;
+        final double flp = forward + right + rotate;
+        final double frp = forward - right - rotate;
+        final double blp = forward - right + rotate;
+        final double brp = forward + right - rotate;
 
         setPowers(flp, frp, blp, brp);
     }
 
     public void driveFieldRelative(double forward, double right, double rotate) {
-        double theta = Math.atan2(forward, right);
-        double r = Math.hypot(forward, right);
+        final double r = Math.hypot(forward, right);
+        final double theta = AngleUnit.normalizeRadians(Math.atan2(forward, right) - getAngle());
 
-        theta = AngleUnit.normalizeRadians(theta - getAngle());
-
-        double newForward = r * Math.sin(theta);
-        double newRight = r * Math.cos(theta);
-
-        drive(newForward, newRight, rotate);
-    }
-
-    public int getDrivePosition(int index){
-        return drivebase[index].getCurrentPosition();
+        drive(r * Math.sin(theta), r * Math.cos(theta), rotate);
     }
 }
