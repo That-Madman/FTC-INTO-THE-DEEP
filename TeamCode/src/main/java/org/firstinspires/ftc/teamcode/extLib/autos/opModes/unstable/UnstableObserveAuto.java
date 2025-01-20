cf218202e21104fe12e028fcae7f64f180677092
That-Madman <144173821+That-Madman@users.noreply.github.com>
2025-01-20 10:04:44 -0500

    Refactoring

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extLib/autos/opModes/unstable/UnstableObserveAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extLib/autos/opModes/unstable/UnstableObserveAuto.java
@@ -5,7 +5,7 @@ import org.firstinspires.ftc.teamcode.extLib.autos.systems.WheelOp;
 
 import Wheelie.Pose2D;
 
-@Autonomous
+@Autonomous (group = "UNSTABLE")
 public class UnstableObserveAuto extends WheelOp {
     private Pose2D[] path1 = new Pose2D[]{
             new Pose2D(0, 0, 0),
