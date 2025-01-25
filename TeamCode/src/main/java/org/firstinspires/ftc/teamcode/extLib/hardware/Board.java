d37f952025bf758b9a08fc53d7d36eef0d7fd34d
That-Madman <144173821+That-Madman@users.noreply.github.com>
2025-01-24 19:18:01 -0500

    WHYYYYYYYYYYYYYYYYYYYYYYYYYYY

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extLib/hardware/Board.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extLib/hardware/Board.java
@@ -215,8 +215,8 @@ public class Board {
                 mRot.setPosition(0);
                 break;
             case 1:
-                lRot.setPosition(1);
-                rRot.setPosition(1);
+                lRot.setPosition(0.9);
+                rRot.setPosition(0.9);
                 mRot.setPosition(0);
                 break;
             case 2:
