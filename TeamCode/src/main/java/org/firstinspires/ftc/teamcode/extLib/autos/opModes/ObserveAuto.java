b518a52cb346498acf6e7abe0eb9ca8c52c6c511
That-Madman <144173821+That-Madman@users.noreply.github.com>
2025-01-25 12:49:53 -0500

    STAYIN' ALIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIVVVVVVVVVVVVVVVVVVVVVVVVEEEEEEE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extLib/autos/opModes/ObserveAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extLib/autos/opModes/ObserveAuto.java
@@ -27,12 +27,12 @@ public class ObserveAuto extends WheelOp {
 
     private Pose2D[] readyPush2 = new Pose2D[]{
             new Pose2D(47,-25,0),
-            new Pose2D(50,-32,0)
+            new Pose2D(50,-33,0)
     };
 
     private Pose2D[] push1 = new Pose2D[] {
-            new Pose2D(50, -32, 0),
-            new Pose2D(3, -32, 0)
+            new Pose2D(50, -33, 0),
+            new Pose2D(3, -33, 0)
     };
 
     private Pose2D[] readyPush3 = new Pose2D[] {
