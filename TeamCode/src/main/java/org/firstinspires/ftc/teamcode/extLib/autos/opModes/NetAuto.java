324fc6151479f255310d0a9ae81bf67f1034930e
That-Madman <144173821+That-Madman@users.noreply.github.com>
2025-01-31 16:00:31 -0500

    Please excuse me, I meant no harm in being born...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extLib/autos/opModes/NetAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extLib/autos/opModes/NetAuto.java
@@ -14,6 +14,16 @@ public class NetAuto extends WheelOp {
             new Pose2D(2, 20)
     };
 
+    Pose2D[] grab1 = new Pose2D[] {
+            new Pose2D(4,18),
+            new Pose2D(6,16),
+    };
+
+    Pose2D[] toBucket2 = new Pose2D[] {
+            new Pose2D(4, 18),
+            new Pose2D(2, 20)
+    };
+
     @Override
     public void onInit() {
         board.setBigGrab(true);
@@ -30,5 +40,56 @@ public class NetAuto extends WheelOp {
         board.setBigGrab(false);
         board.setTinyGrab(false);
         sleep(500);
+
+        board.setLift(0);
+
+        followPath(grab1);
+
+        board.setReach((byte) 1);
+
+        board.setPick(false);
+        board.setSwivel((byte) 2);
+
+        { //Block of Handoff
+            board.setRot((byte) 0);
+            board.setBigGrab(false);
+            board.setTinyGrab(false);
+
+            board.setPick(false);
+            sleep(150);
+
+            board.setReach((byte) 1);
+            sleep(150);
+
+            board.setSwivel((byte) 0);
+            sleep(150);
+
+            board.setReach((byte) 0);
+            sleep(150);
+
+            board.setTinyGrab(true);
+            sleep(150);
+
+            board.setPick(true);
+            sleep(150);
+
+            board.setReach((byte) 1);
+            sleep(150);
+
+            board.setBigGrab(true);
+            sleep(150);
+
+            board.setRot((byte) 2);
+        }
+
+        board.setLift(2700);
+
+        followPath(toBucket2);
+
+        board.setBigGrab(false);
+        board.setTinyGrab(false);
+        sleep(500);
+
+        board.setLift(0);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extLib/autos/opModes/ObserveAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extLib/autos/opModes/ObserveAuto.java
@@ -21,44 +21,44 @@ public class ObserveAuto extends WheelOp {
 
     final private Pose2D[] readyPush1 = new Pose2D[] {
             new Pose2D(3, 0),
-            new Pose2D(3, -17),
-            new Pose2D(47, -17)
+            new Pose2D(3, -27),
+            new Pose2D(47, -27)
     };
 
     final private Pose2D[] readyPush2 = new Pose2D[] {
-            new Pose2D(47,-17),
-            new Pose2D(50,-27)
+            new Pose2D(47,-27),
+            new Pose2D(50,-37)
     };
 
     final private Pose2D[] push1 = new Pose2D[] {
-            new Pose2D(50, -25),
-            new Pose2D(3, -25)
-    };
-
-    final private Pose2D[] readyPush3 = new Pose2D[] {
-            new Pose2D(0, -30),
-            new Pose2D(25, -27),
-            new Pose2D(50, -30)
-    };
-
-    final private Pose2D[] push2 = new Pose2D[] {
-            new Pose2D(50, -25),
-            new Pose2D(50, -32),
+            new Pose2D(50, -35),
             new Pose2D(3, -35)
     };
 
+    final private Pose2D[] readyPush3 = new Pose2D[] {
+            new Pose2D(0, -35),
+            new Pose2D(25, -35),
+            new Pose2D(50, -35)
+    };
+
+    final private Pose2D[] push2 = new Pose2D[] {
+            new Pose2D(50, -35),
+            new Pose2D(50, -42),
+            new Pose2D(3, -42)
+    };
+
     final private Pose2D[] readyStrafeToScore1 = new Pose2D[] {
-            new Pose2D(2, -35),
-            new Pose2D(2, -18)
+            new Pose2D(6, -42),
+            new Pose2D(6, -28)
     };
 
     final private Pose2D[] strafeToScore1 = new Pose2D [] {
-            new Pose2D(3, -18),
-            new Pose2D(1, -18)
+            new Pose2D(5, -28),
+            new Pose2D(1, -28)
     };
 
     final private Pose2D[] toSubmersible = new Pose2D[] {
-            new Pose2D(6, -15),
+            new Pose2D(6, -28),
             new Pose2D(6, 4),
             new Pose2D(19, 4)
     };
