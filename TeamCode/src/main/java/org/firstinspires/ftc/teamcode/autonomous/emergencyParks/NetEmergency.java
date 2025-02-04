d7302796f1bd0df952d9247e8575f52a64779e65
That-Madman <144173821+That-Madman@users.noreply.github.com>
2025-02-04 18:31:36 -0500

    Don't stop believing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/emergencyParks/NetEmergency.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/emergencyParks/NetEmergency.java
@@ -10,7 +10,7 @@ import Wheelie.Pose2D;
 @Autonomous(group = "Emergency")
 public class NetEmergency extends WheelOp {
     final Pose2D[] park = new Pose2D[] {
-            new Pose2D(22, 22),
+            new Pose2D(22, 40),
             new Pose2D(52, 29),
             new Pose2D(55, 14),
             new Pose2D(53, 8)
