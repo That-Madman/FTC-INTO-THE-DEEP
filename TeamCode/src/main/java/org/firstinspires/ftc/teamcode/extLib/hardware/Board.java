a615953b2c8cdd6e8f3c273a3765f180a77a9e2f
PlagueDoctorSadMan <karakashg@franklinacademy.org>
2025-02-05 15:30:30 -0500

    fixed by Alex

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extLib/hardware/Board.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extLib/hardware/Board.java
@@ -11,6 +11,8 @@ import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.hardware.ColorRangeSensor;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
@@ -299,11 +301,21 @@ public class Board {
         }
     }
 
-    public void genericCameraThingy() {
+    public void genericCameraThingy(Telemetry telemetry) {
+        List <AprilTagDetection> currentDetections = aprilTagProcessor.getDetections();
         StringBuilder idsFound = new StringBuilder();
           for (AprilTagDetection detection : currentDetections) {
              idsFound.append(detection.id);
              idsFound.append(' ');
+              if (detection.metadata != null) {
+                  telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                  telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                  telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                  telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+              } else {
+                  telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                  telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+              }
 
     }}
 
